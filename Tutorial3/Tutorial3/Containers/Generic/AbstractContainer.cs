55fa6aaea4be21f92a0ad91dce865257659c06d9
Cezary Petryka <cezaryp10@gmail.com>
2024-03-23 14:56:04 +0100

    Add abstract class for all types of containers

---
+++ Tutorial3/Tutorial3/Containers/Generic/AbstractContainer.cs
@@ -0,0 +1,37 @@
+using Tutorial3.Exceptions;
+
+namespace Tutorial3.Containers;
+
+public abstract class AbstractContainer : IContainer
+{
+    protected double CargoWeight { get; set; }
+    protected double Height { get; set; }
+    protected double CurbWeight { get; set; }
+    protected double Depth { get; set; }
+    protected string SerialNumber { get; set; }
+    protected double MaxCargoWeight { get; set; }
+
+    protected AbstractContainer(double height, double curbWeight, double depth, string serialNumber, double maxCargoWeight)
+    {
+        Height = height;
+        CurbWeight = curbWeight;
+        Depth = depth;
+        SerialNumber = serialNumber;
+        MaxCargoWeight = maxCargoWeight;
+    }
+
+    public virtual void Load(double cargoWeight)
+    {
+        if (cargoWeight > MaxCargoWeight)
+        {
+            throw new OverfillException("Cargo weight exceeds the maximum allowed weight.");
+        }
+
+        CargoWeight = cargoWeight;
+    }
+
+    public virtual void Unload()
+    {
+        CargoWeight = 0;
+    }
+}
